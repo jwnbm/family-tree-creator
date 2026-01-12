224c29e987ad8204fd0500985f1c3eb548a12026
j-watanabe <jwnbm.1255@gmail.com>
2026-01-12 23:56:18 +0900

    Noto Sans JPフォントを使用して日本語表示を改善
    - fonts/NotoSansJP-Regular.ttfを追加
    - setup_fonts関数でNoto Sans JPを読み込み
    - Proportionalフォントファミリーの最優先に設定
    - 日本語が確実に表示されるようになった

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,45 @@ use app::App;
 fn main() -> eframe::Result<()> {
     let options = eframe::NativeOptions {
         viewport: eframe::egui::ViewportBuilder::default()
-            .with_title("Family Tree (egui MVP)")
+            .with_title("Family Tree")
             .with_inner_size([1100.0, 700.0]),
         ..Default::default()
     };
     eframe::run_native(
-        "Family Tree (egui MVP)",
+        "Family Tree",
         options,
-        Box::new(|_cc| Ok(Box::new(App::default()))),
+        Box::new(|cc| {
+            // 日本語フォントが含まれるようにする
+            setup_fonts(&cc.egui_ctx);
+            Ok(Box::new(App::default()))
+        }),
     )
+}
+
+fn setup_fonts(ctx: &eframe::egui::Context) {
+    let mut fonts = eframe::egui::FontDefinitions::default();
+    
+    // Noto Sans JPフォントを追加
+    fonts.font_data.insert(
+        "noto_sans_jp".to_owned(),
+        std::sync::Arc::new(eframe::egui::FontData::from_static(include_bytes!(
+            "../fonts/NotoSansJP-Regular.ttf"
+        ))),
+    );
+    
+    // Proportionalフォントファミリーの最優先に設定
+    fonts
+        .families
+        .entry(eframe::egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "noto_sans_jp".to_owned());
+    
+    // Monospaceフォントファミリーにも追加
+    fonts
+        .families
+        .entry(eframe::egui::FontFamily::Monospace)
+        .or_default()
+        .push("noto_sans_jp".to_owned());
+    
+    ctx.set_fonts(fonts);
 }
